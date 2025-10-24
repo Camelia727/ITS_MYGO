5eb5dbf40f1d1d80fbfc45378d2cbd2305db589d
Camelia727 <231880343@smail.nju.edu.cn>
2025-10-24 16:16:49 +0800

    project init

---
+++ src/main/java/com/example/MY_Goal_Optimizer/MyGoalOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.MY_Goal_Optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGoalOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGoalOptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MY_Goal_Optimizer/MyGoalOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MY_Goal_Optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGoalOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
